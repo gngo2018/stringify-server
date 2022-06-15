d69e53783a4fa94e330d6882b8c941395e511614
George <gngo2018@gmail.com>
2022-06-15 01:49:27 -0400

    Added field property to model to match database columns

--- src/db/models/Client.ts
+++ src/db/models/Client.ts
@@ -2,52 +2,67 @@ import { DataTypes, Model, Optional } from 'sequelize'
 import sequelizeConnection from '../config'
 
 interface ClientAttributes {
-    id: number;
-    firstName: string;
-    lastName: string;
-    racket: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-    deletedAt?: Date;
+  id: number;
+  firstName: string;
+  lastName: string;
+  racket: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
 }
 
-export interface ClientInput extends Optional<ClientAttributes, 'id' | 'firstName' | 'lastName' | 'racket'> {}
-export interface ClientOuput extends Required<ClientAttributes> {}
+export interface ClientInput extends Optional<ClientAttributes, 'id' | 'firstName' | 'lastName' | 'racket'> { }
+export interface ClientOuput extends Required<ClientAttributes> { }
 
 class Client extends Model<ClientAttributes, ClientInput> implements ClientAttributes {
-    public id!: number
-    public firstName!: string
-    public lastName!: string
-    public racket!: string
-  
-    // timestamps!
-    public readonly createdAt!: Date;
-    public readonly updatedAt!: Date;
-    public readonly deletedAt!: Date;
+  public id!: number
+  public firstName!: string
+  public lastName!: string
+  public racket!: string
+
+  // timestamps!
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+  public readonly deletedAt!: Date;
 }
 
 Client.init({
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    racket: {
-      type: DataTypes.TEXT
-    }
-  }, {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'first_name'
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    field: 'last_name'
+  },
+  racket: {
+    type: DataTypes.TEXT
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    field: 'created_at'
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    field: 'updated_at'
+  },
+  deletedAt: {
+    type: DataTypes.DATE,
+    field: 'deleted_at'
+  }
+},
+  {
     timestamps: true,
     sequelize: sequelizeConnection,
     paranoid: true
   })
-  
-  export default Client;
+
+export default Client;
