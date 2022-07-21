dffeeae5a884b48231a52fcb8d30c178178f8c80
George <gngo2018@gmail.com>
2022-07-21 17:48:33 -0400

    Added conditional for using ssl with sequelize config

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,20 +1,26 @@
 import { Dialect, Sequelize } from 'sequelize'
-
+const environment = process.env.NODE_ENV;
 const dbName = process.env.DB_NAME as string
 const dbUser = process.env.DB_USER as string
 const dbHost = process.env.DB_HOST
 const dbDialect = process.env.DB_DIALECT as Dialect
 const dbPassword = process.env.DB_PASSWORD
 
-const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
-  host: dbHost,
-  dialect: dbDialect,
-  dialectOptions: {
+let dialectOptions = {}
+
+if(environment !== 'Development'){
+  dialectOptions = {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
   }
+}
+
+const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  dialect: dbDialect,
+  dialectOptions: dialectOptions
 })
 
 export default sequelizeConnection
